3e1dcd56e1b60ed22991f11766370dbc57814370
heru-h <49828519+heru-h@users.noreply.github.com>
2019-06-16 20:31:59 +0800

    Add files via upload

---
+++ OtherLayer.h
@@ -0,0 +1,22 @@
+/*
+    文件名：    OtherLayer.h
+    描　述：    另外一个层
+    创建人：    笨木头_钟迪龙 (博客：http://www.benmutou.com)
+*/
+#ifndef _OtherLayer_H_
+#define _OtherLayer_H_
+
+#include "cocos2d.h"
+USING_NS_CC;
+
+class OtherLayer : public Layer {
+public:
+	CREATE_FUNC(OtherLayer);
+	virtual bool init();
+
+private:
+	/* 接收test消息的回调函数 */
+	void testMsg(Ref* pData);
+};
+
+#endif
